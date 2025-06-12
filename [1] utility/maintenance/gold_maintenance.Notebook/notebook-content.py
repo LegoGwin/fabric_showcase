ac5a350967e56f8d2f06ba281e7d30103f38883a
LegoGwin <43446450+LegoGwin@users.noreply.github.com>
2025-06-12 16:34:32 -0400

    Committing 2 items from workspace 1d10f168-5ee0-487f-bfb4-4bc7e9fdb6ab

---
+++ [1] utility/maintenance/gold_maintenance.Notebook/notebook-content.py
@@ -0,0 +1,32 @@
+# Fabric notebook source
+
+# METADATA ********************
+
+# META {
+# META   "kernel_info": {
+# META     "name": "synapse_pyspark"
+# META   },
+# META   "dependencies": {
+# META     "lakehouse": {
+# META       "default_lakehouse": "31d88d62-8db6-461e-b773-aa3396d3db9d",
+# META       "default_lakehouse_name": "gold_lakehouse",
+# META       "default_lakehouse_workspace_id": "1d10f168-5ee0-487f-bfb4-4bc7e9fdb6ab",
+# META       "known_lakehouses": [
+# META         {
+# META           "id": "31d88d62-8db6-461e-b773-aa3396d3db9d"
+# META         }
+# META       ]
+# META     }
+# META   }
+# META }
+
+# CELL ********************
+
+%run maintenance
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
