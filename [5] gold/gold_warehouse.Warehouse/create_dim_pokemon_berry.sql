e6d7005ebc9f9869935fd24a5fd6a1a881f8c036
LegoGwin <43446450+LegoGwin@users.noreply.github.com>
2025-07-03 14:57:23 -0400

    Committing 1 item from workspace 1d10f168-5ee0-487f-bfb4-4bc7e9fdb6ab

---
+++ [5] gold/gold_warehouse.Warehouse/create_dim_pokemon_berry.sql
@@ -0,0 +1,2 @@
+create or alter view [dbo].[DimPokemonBerry] as
+select * from [gold_lakehouse].[dbo].[DimPokemonBerry]
---
+++ [5] gold/gold_warehouse.Warehouse/dbo/Views/DimPokemonBerry.sql
@@ -0,0 +1,3 @@
+-- Auto Generated (Do not modify) 1645D2CF0514CC293972B83891D4BE0F7CCD04B850C7A8854D4D17E1FDB2924D
+create   view [dbo].[DimPokemonBerry] as
+select * from [gold_lakehouse].[dbo].[DimPokemonBerry]
